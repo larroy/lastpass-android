65e914f235096d538b816313ab9bd1d6c3b6ad52
Pedro Larroy <pedro.larroy@gmail.com>
2014-01-02 00:19:02 +0100

    Setting up an ActivityInstrumentationTestCase2

--- lastpass-android-test/src/com/larroy/loginactivitytest/LoginActivityTest.java
+++ lastpass-android-test/src/com/larroy/loginactivitytest/LoginActivityTest.java
@@ -1,23 +1,28 @@
 package com.larroy.loginactivitytest;
 
+import com.nhinds.lastpass.android;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LoginActivityTest extends Activity {
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+    private Solo solo;
+
+    public LoginActivityTest() {
+      super(LoginActivity.class);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activity_test);
+    public void setUp() throws Exception {
+      solo = new Solo(getInstrumentation(), getActivity());
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login_activity_test, menu);
-        return true;
+    public void tearDown() throws Exception {
+      solo.finishOpenedActivities();
     }
-    
 }
